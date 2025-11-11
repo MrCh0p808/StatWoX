1622f81b85c3211fe4b36c469b452836e43f02a3
Trideev Ganguly <trideevg7@gmail.com>
2025-11-11 04:03:07 +0000

    fix : CDN + OAC needs no public Getobject

--- infra/s3.tf
+++ infra/s3.tf
@@ -29,11 +29,18 @@ resource "aws_s3_bucket_policy" "frontend" {
     Version = "2012-10-17",
     Statement = [
       {
-        Sid : "PublicReadGetObject",
-        Effect : "Allow",
-        Principal : "*",
-        Action : ["s3:GetObject"],
-        Resource : ["${aws_s3_bucket.frontend.arn}/*"]
+        Sid      = "AllowCloudFrontServicePrincipalReadOnly",
+        Effect   = "Allow",
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        },
+        Action   = "s3:GetObject",
+        Resource = ["${aws_s3_bucket.frontend.arn}/*"],
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.frontend_cdn.arn
+          }
+        }
       }
     ]
   })
